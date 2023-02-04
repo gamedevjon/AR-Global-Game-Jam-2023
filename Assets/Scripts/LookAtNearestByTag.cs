13287155c3680bf12a9d905f872133c3e3a4e92e
BT100 Building <BT100@ac.sdmesa.net>
2023-02-04 14:06:22 -0800

    tower-poc

---
+++ Assets/Scripts/LookAtNearestByTag.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtNearestByTag : MonoBehaviour
+{
+
+    public GameObject target;
+    public string lookTag = "Enemy";
+    public float lookSpeed = 5f;
+    public float lookDist = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Find nearest target
+        GameObject[] candidates = GameObject.FindGameObjectsWithTag(lookTag);
+        float distNearest = float.PositiveInfinity;
+        target = null;
+        foreach (GameObject candidate in candidates)
+        {
+            Vector3 pos = candidate.transform.position - transform.position;
+            float dist = pos.magnitude;
+            if(dist < lookDist)
+            {
+                if (dist < distNearest)
+                {
+                    distNearest = dist;
+                    target = candidate;
+                }
+            }
+        }
+
+        // Look at target
+        if(target)
+        {
+
+            // Look direction
+            Vector3 lookPos = target.transform.position - transform.position;
+            lookPos.y = 0f;
+
+            // Smoothly rotate towards the target point
+            Quaternion targetRotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
+
+        }
+
+    }
+
+}
